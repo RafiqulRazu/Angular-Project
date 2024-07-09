93e776bba368d54e1244a90681606eb6b0364c68
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2024-07-10 04:29:03 +0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'student', component:StudentComponent},
+  {path: 'location', component:LocationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
